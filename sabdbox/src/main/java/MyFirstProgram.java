70e165bbddb19408acbdb47cc2600d72d844fe3d
Sergey <sergeymorf4r@gmail.com>
2018-11-21 21:23:17 +0300

    Поменял выводимый текст

--- sabdbox/src/main/java/MyFirstProgram.java
+++ sabdbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,7 @@
 public class MyFirstProgram{
 
     public static void main(String[] args){
-        System.out.println("Hello, world");
+        System.out.println("Hello, world. This is my first program on a long but interesting learning path.");
     }
 
 }
