a82e44ee41375539031f1a4284b50b5fcd322f19
Sergey <sergeymorf4r@gmail.com>
2018-11-21 21:39:41 +0300

    Создали пакет

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,4 +1,6 @@
-public class MyFirstProgram{
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
 
     public static void main(String[] args){
         System.out.println("Hello, world. This is my first program on a long but interesting learning path.");
