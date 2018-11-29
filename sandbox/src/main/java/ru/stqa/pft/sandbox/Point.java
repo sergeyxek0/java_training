cd4b1de71b0805cadd1ecb36c93f059f2d586292
Sergey <sergeymorf4r@gmail.com>
2018-11-29 21:41:51 +0300

    Убрал комментарии в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -10,10 +10,6 @@ public class Point {
       this.y = y;
     }
 
-  //public double distance(){
-  //  return Math.sqrt((x2 - x1)*(x2 - x1)+(y2 - y1)*(y2 - y1));
-  //}
-
     public double distance(Point p2){
      return Math.sqrt((this.x - p2.x)*(this.x - p2.x)+(this.y - p2.y)*(this.y - p2.y));
    }
