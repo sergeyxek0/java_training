8817a0b1656f4006b20ea4afd67b0afdf0f340e6
Sergey <sergeymorf4r@gmail.com>
2019-02-13 22:44:42 +0300

    Общий базовый класс

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -0,0 +1,33 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+
+public class HelperBase {
+  protected WebDriver wd;
+
+  public HelperBase(WebDriver wd) {
+    this.wd = wd;
+  }
+
+  protected void click(By locator) {
+    wd.findElement(locator).click();
+  }
+
+  protected void type(By Locator, String text) {
+    click(Locator);
+    wd.findElement(Locator).clear();
+    wd.findElement(Locator).sendKeys(text);
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+}
